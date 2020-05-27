da1e4c82c8f35e4d6cc926555278924e49e8cce1
Bathina Mounika <62980926+bathinaMounika@users.noreply.github.com>
2020-05-27 10:13:13 +0530

    handled edge cases like empty array

--- SearchInRotatedSortedArray.kt
+++ SearchInRotatedSortedArray.kt
@@ -1,19 +1,12 @@
-fun main(args: Array<String>){
-    val solution: Solution = Solution()
-    println("performing search ..target found at index " + solution.search(intArrayOf(4,5,6,7,0,1,2), 3))
-    println("pivot found at " + solution.getPivot(intArrayOf(4,5,6,7,0,1,2)))
-}
-
-
 class Solution {
 
     fun search(nums: IntArray, target: Int): Int {
-        val pivot = getPivot(nums)
-        if (pivot !=-1) when(target){
+        var pivot = getPivot(nums)
+        if(nums.size != 0 && pivot != -1) when(target){
             in nums[0]..nums[pivot -1] -> return binarySearch(nums.copyOfRange(0, pivot), target)
             in nums[pivot]..nums[nums.lastIndex] -> {
                 var t = binarySearch(nums.copyOfRange(pivot, nums.size), target)
-                println("printing t is ${t}")
+                //println("printing t is ${t}")
                 if(t != -1){
                     return pivot + t
                 }
@@ -22,11 +15,15 @@ class Solution {
                 }
             }
         }
+        if(nums.size!=0 && pivot == -1){
+            return  binarySearch(nums, target)
+        }
         return -1
     }
 
     fun getPivot(nums: IntArray):Int{
         //returns the index at which the sorted array is rotated, else -1
+        if(nums.size == 0) return -1
         var prev: Int = nums[0]
         for(index in nums.indices){
             if (prev > nums[index]){
@@ -42,7 +39,7 @@ class Solution {
     fun binarySearch(nums: IntArray, target: Int): Int{
         //returns the index of the target if found, else -1
         //expecting it will work with O(n) time complexity
-        println("performing binary search on" + nums.asList())
+        //println("performing binary search on" + nums.asList())
         if(nums.size > 1) {
             var split = nums.size / 2
             if (nums[split] == target){
@@ -50,12 +47,12 @@ class Solution {
             }
             if(nums[split] > target){
                 var t = binarySearch(nums.copyOfRange(0,split), target)
-                println("printing t ${t} for ${nums.asList()}")
+                //println("printing t ${t} for ${nums.asList()}")
                 if (t == -1) return  -1 else return t
             }
             if (nums[split] < target){
                 var t = binarySearch(nums.copyOfRange(split , nums.size), target)
-                println("printing t ${t} for ${nums.asList()}")
+                //println("printing t ${t} for ${nums.asList()}")
                 if(t == -1) return -1 else return split + t
             }
         }
